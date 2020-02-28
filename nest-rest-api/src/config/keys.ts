9f867654529bd28020269a58167b63c445e9db89
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-28 20:26:50 +0800

    modified:   nest-rest-api/package-lock.json         modified:   nest-rest-api/package.json         modified:   nest-rest-api/src/app.module.ts         new file:   nest-rest-api/src/config/keys.ts         new file:   nest-rest-api/src/items/interfaces/item.interface.ts         modified:   nest-rest-api/src/items/items.controller.ts         new file:   nest-rest-api/src/items/items.module.ts         new file:   nest-rest-api/src/items/items.service.spec.ts         new file:   nest-rest-api/src/items/items.service.ts         new file:   nest-rest-api/src/items/schemas/item.schema.ts

---
+++ nest-rest-api/src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb+srv://syakirharis:syakirmongodb@cluster0-w5mqd.mongodb.net/test?retryWrites=true&w=majority'
+};
--- nest-rest-api/src/items/items.controller.ts
+++ nest-rest-api/src/items/items.controller.ts
@@ -8,21 +8,35 @@ import {
     Param,
 } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService: ItemsService) { }
+
     @Get()
-    findAll(): string {
-        return 'Get all items';
+    async findAll(): Promise<Item[]> {
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id): string {
-        return `Item ${id}`;
+    async findOne(@Param('id') id): Promise<Item> {
+        return this.itemsService.findOne(id);
     }
 
     @Post()
     create(@Body() createItemDto: CreateItemDto): string {
         return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): string {
+        return `Delete ${id}`;
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+        return `Update ${id} - Name: ${updateItemDto.name}`;
     }
 }
